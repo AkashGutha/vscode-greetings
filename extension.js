7ed4fe0be6ac19acf64d95e937a9eaf71209a92f
AkashGutha <akash.gutha@outlook.com>
2016-10-27 17:51:03 +0530

    add errors and information logic

--- extension.js
+++ extension.js
@@ -2,10 +2,23 @@
 // Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
 let unirest = require('unirest');
+let fs = require('fs');
 
-/*let greetings = JSON.parse(fs.readFile('./json/greetings.json', function (error) {
-    console.log(error);
-}));*/
+let greetings = {
+    "greets": [
+        "Hello Coder!",
+        "Hello Programmer!",
+        "Hello Geek!",
+        "Hello TechGeek!",
+        "Hello Coder!"
+    ],
+    "wishes": [
+        "Have a great day ahead.",
+        "Have a wonderful day.",
+        "Happy programming.",
+        "Have a marvelous day."
+    ]
+};
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -14,24 +27,26 @@ function activate(context) {
     let greet = vscode.commands.registerCommand('extension.greet', function () {
         // The code you place here will be executed every time your command is executed
 
+        let randomGreet = Math.floor(Math.random() * greetings.greets.length);
+        let randomWish = Math.floor(Math.random() * greetings.wishes.length);
+
         // These code snippets use an open-source library. http://unirest.io/nodejs
         unirest.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
             .header("X-Mashape-Key", "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
             .header("Content-Type", "application/x-www-form-urlencoded")
             .header("Accept", "application/json")
             .end(function (result) {
-                console.log(result);
+                if (typeof result.error === 'object')
+                    console.log('errored');
+                else
+                    console.log(result);
                 console.log(result.status, result.headers, result.body);
                 vscode.window.showInformationMessage(result.body);
             });
 
-        const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, 0);
-        item.text = 'akash';
+        const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
+        item.text = '${content} ' + '$(sync)';
         item.show();
-
-        //let randomGreet = Math.floor(Math.random() * greetings.greets.length);
-        //let randomWish = Math.floor(Math.random() * greetings.wishes.length);
-
     });
 
     context.subscriptions.push(greet);
