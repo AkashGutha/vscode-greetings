900c772ff5ab62c0b12a5e610f7da81bc1a01b18
AkashGutha <akash.gutha@outlook.com>
2016-10-27 22:16:15 +0530

    add network check and streamlined functions

--- extension.js
+++ extension.js
@@ -2,20 +2,14 @@
 // Import the module and reference it with the alias vscode in your code below
 let vscode = require('vscode');
 let unirest = require('unirest');
-let fs = require('fs');
 
 let greetings = {
     "greets": [
-        "Hello Coder!",
-        "Hello Programmer!",
-        "Hello Geek!",
-        "Hello TechGeek!",
+        "Hello Coder!", "Hello Programmer!", "Hello Geek!", "Hello TechGeek!",
         "Hello Coder!"
     ],
     "wishes": [
-        "Have a great day ahead.",
-        "Have a wonderful day.",
-        "Happy programming.",
+        "Have a great day ahead.", "Have a wonderful day.", "Happy programming.",
         "Have a marvelous day."
     ]
 };
@@ -23,30 +17,35 @@ let greetings = {
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
-
     let greet = vscode.commands.registerCommand('extension.greet', function () {
-        // The code you place here will be executed every time your command is executed
+        // The code you place here will be executed every time your command is
+        // executed
 
-        let randomGreet = Math.floor(Math.random() * greetings.greets.length);
-        let randomWish = Math.floor(Math.random() * greetings.wishes.length);
+        let content = "";
 
         // These code snippets use an open-source library. http://unirest.io/nodejs
-        unirest.post("https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
-            .header("X-Mashape-Key", "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .header("Accept", "application/json")
-            .end(function (result) {
-                if (typeof result.error === 'object')
-                    console.log('errored');
-                else
-                    console.log(result);
-                console.log(result.status, result.headers, result.body);
-                vscode.window.showInformationMessage(result.body);
-            });
+        if (hostReachable()) {
+            unirest
+                .post(
+                "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
+                .header(
+                "X-Mashape-Key",
+                "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .header("Accept", "application/json")
+                .end(function (result) {
+                    if (typeof result.error === 'object') {
+                        content = getRandom();
+                    } else
+                        content = JSON.parse(result.body).quote;
 
-        const item = vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Right, 100);
-        item.text = '${content} ' + '$(sync)';
-        item.show();
+                });
+        } else {
+            content = getRandom();
+            setStatusBarItem(content);
+        }
+
+
     });
 
     context.subscriptions.push(greet);
@@ -54,7 +53,31 @@ function activate(context) {
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {
-
-}
+function deactivate() { }
 exports.deactivate = deactivate;
+
+
+// set status bar item
+function setStatusBarItem(content) {
+    let item =
+        vscode.window.createStatusBarItem(vscode.StatusBarAlignment.Left, -100);
+    item.text = content + '   $(sync)';
+    item.show();
+}
+// get random content
+function getRandom() {
+    let randomGreet = Math.floor(Math.random() * greetings.greets.length);
+    let randomWish = Math.floor(Math.random() * greetings.wishes.length);
+    return randomGreet + " " + randomWish;
+}
+
+// check for internet connection.
+function hostReachable() {
+    require('dns').resolve('www.google.com', function (err) {
+        if (err) {
+            return false;
+        } else {
+            return true;
+        }
+    });
+}
