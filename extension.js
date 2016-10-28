de0c3e3b98a387877a55a4d65052d2a80d0265fc
AkashGutha <akash.gutha@outlook.com>
2016-10-28 21:06:04 +0530

    formatting changes

--- extension.js
+++ extension.js
@@ -4,80 +4,79 @@ let vscode = require('vscode');
 let unirest = require('unirest');
 
 let greetings = {
-    "greets": [
-        "Hi Coder!", "Hi Programmer!", "Hi Geek!", "Hi Techie!",
-        "Hi Coder!", "Hello Coder!", "Hello Programmer!", "Hello Geek!", "Hello Techie!",
-        "Hello Coder!"
-    ],
-    "wishes": [
-        "Have a great day ahead.", "Have an awesome coding day.", "Happy programming.",
-        "Have a great day."
-    ]
+  "greets": [
+    "Hi Coder!", "Hi Programmer!", "Hi Geek!", "Hi Techie!", "Hi Coder!",
+    "Hello Coder!", "Hello Programmer!", "Hello Geek!", "Hello Techie!",
+    "Hello Coder!"
+  ],
+  "wishes": [
+    "Have a great day ahead.", "Have an awesome coding day.",
+    "Happy programming.", "Have a great day."
+  ]
 };
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 function activate(context) {
+  // register the command to be called from command palette.
+  vscode.commands.registerCommand('extension.greet', greet());
 
+  // greet function which gets the data and sets the status bar message.
+  function greet() {
+    // The code you place here will be executed every time your command is
+    // executed
+    console.log('activated');
 
-    // register the command to be called from command palette.
-    vscode.commands.registerCommand('extension.greet', greet());
+    let content = "";
+    let dns = require('dns');
 
-    // greet function which gets the data and sets the status bar message.
-    function greet() {
-        // The code you place here will be executed every time your command is executed
-        console.log('activated');
+    dns.resolve('www.google.com', function(err) {
+      if (err) {
+        content = getRandom();
 
-        let content = "";
-        let dns = require('dns');
-
-        dns.resolve('www.google.com', function (err) {
-            if (err) {
+        // set status bar messgae
+        vscode.window.setStatusBarMessage(content, 15000);
+      } else {
+        // These code snippets use an open-source library.
+        // http://unirest.io/nodejs
+        unirest
+            .post(
+                "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
+            .header(
+                "X-Mashape-Key",
+                "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
+            .header("Content-Type", "application/x-www-form-urlencoded")
+            .header("Accept", "application/json")
+            .end(function(result) {
+              if (typeof result.error === 'object')
                 content = getRandom();
+              else
+                content = JSON.parse(result.body).quote;
 
-                //set status bar messgae
-                vscode.window.setStatusBarMessage(content, 15000);
-            } else {
-                // These code snippets use an open-source library. http://unirest.io/nodejs
-                unirest
-                    .post(
-                    "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
-                    .header(
-                    "X-Mashape-Key",
-                    "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
-                    .header("Content-Type", "application/x-www-form-urlencoded")
-                    .header("Accept", "application/json")
-                    .end(function (result) {
-                        if (typeof result.error === 'object')
-                            content = getRandom();
-                        else
-                            content = JSON.parse(result.body).quote;
+              // set status bar messgae
+              vscode.window.setStatusBarMessage(content, 15000);
+            });
+      }
+    });
 
-                        //set status bar messgae
-                        vscode.window.setStatusBarMessage(content, 15000);
-                    });
-            }
-        });
+    context.subscriptions.push(content);
+    context.subscriptions.push(dns);
 
-        context.subscriptions.push(content);
-        context.subscriptions.push(dns);
-
-        //call for the first time vscode is activated.
-        greet();
-
-    }
+    // call for the first time vscode is activated.
+    // greet();
+  }
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() { }
+function deactivate() {}
 exports.deactivate = deactivate;
 
 // get random content
 function getRandom() {
-    let randomGreet =
-        greetings.greets[Math.floor(Math.random() * greetings.greets.length)];
-    let randomWish =
-        greetings.wishes[Math.floor(Math.random() * greetings.wishes.length)];
-    return randomGreet + " " + randomWish;
+  let randomGreet =
+      greetings.greets[Math.floor(Math.random() * greetings.greets.length)];
+  let randomWish =
+      greetings.wishes[Math.floor(Math.random() * greetings.wishes.length)];
+  return randomGreet + " " + randomWish;
 }
