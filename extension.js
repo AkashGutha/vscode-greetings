7a69216e45a18a653d800f3c06cbba81b0f347cd
AkashGutha <akash.gutha@outlook.com>
2016-10-28 22:38:28 +0530

    add uinrest

--- extension.js
+++ extension.js
@@ -19,64 +19,57 @@ let greetings = {
 // your extension is activated the very first time the command is executed
 function activate(context) {
   // register the command to be called from command palette.
-  vscode.commands.registerCommand('extension.greet', greet());
+  var greetCommand =
+    vscode.commands.registerCommand('GreetsnQuotes.greet', greet());
 
   // greet function which gets the data and sets the status bar message.
   function greet() {
     // The code you place here will be executed every time your command is
     // executed
-    console.log('activated');
-
     let content = "";
     let dns = require('dns');
 
-    dns.resolve('www.google.com', function(err) {
+    dns.resolve('www.google.com', function (err) {
       if (err) {
         content = getRandom();
-
         // set status bar messgae
-        vscode.window.setStatusBarMessage(content, 15000);
+        vscode.window.setStatusBarMessage(content, 25000);
       } else {
         // These code snippets use an open-source library.
         // http://unirest.io/nodejs
         unirest
-            .post(
-                "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
-            .header(
-                "X-Mashape-Key",
-                "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
-            .header("Content-Type", "application/x-www-form-urlencoded")
-            .header("Accept", "application/json")
-            .end(function(result) {
-              if (typeof result.error === 'object')
-                content = getRandom();
-              else
-                content = JSON.parse(result.body).quote;
+          .post(
+          "https://andruxnet-random-famous-quotes.p.mashape.com/?cat=famous")
+          .header(
+          "X-Mashape-Key",
+          "lNzJ60W1wfmshdbNCHarQVa2yOzYp1GCICRjsnsIhFM5zUuokz")
+          .header("Content-Type", "application/x-www-form-urlencoded")
+          .header("Accept", "application/json")
+          .end(function (result) {
+            if (typeof result.error === 'object')
+              content = getRandom();
+            else
+              content = JSON.parse(result.body).quote;
 
-              // set status bar messgae
-              vscode.window.setStatusBarMessage(content, 15000);
-            });
+            // set status bar messgae
+            vscode.window.setStatusBarMessage(content, 25000);
+          });
       }
     });
-
-    context.subscriptions.push(content);
-    context.subscriptions.push(dns);
-
-    // call for the first time vscode is activated.
-    // greet();
+    context.subscriptions.push(greetCommand);
   }
 }
 exports.activate = activate;
 
 // this method is called when your extension is deactivated
-function deactivate() {}
+function deactivate() { }
 exports.deactivate = deactivate;
 
 // get random content
 function getRandom() {
   let randomGreet =
-      greetings.greets[Math.floor(Math.random() * greetings.greets.length)];
+    greetings.greets[Math.floor(Math.random() * greetings.greets.length)];
   let randomWish =
-      greetings.wishes[Math.floor(Math.random() * greetings.wishes.length)];
+    greetings.wishes[Math.floor(Math.random() * greetings.wishes.length)];
   return randomGreet + " " + randomWish;
 }
